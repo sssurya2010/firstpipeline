df8536864dcc7783e4a0986a1bef438435288d66
Surya Sivakumar <sssurya2010@gmail.com>
2025-07-31 12:27:55 -0700

    add gcs_to_bq dag 2

--- DAG/gcs_to_bq_dag.py
+++ DAG/gcs_to_bq_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-from airflow.providers.google.cloud.operators.gcs import GCSToGCSOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
 default_args = {
@@ -9,11 +10,20 @@ default_args = {
     'retries': 0,
 }
 
+def move_to_processed_func(execution_date, **context):
+    gcs_hook = GCSHook()
+    src_bucket = 'firstworkflow'
+    src_object = 'output/country_wise_temp.csv'
+    dest_object = f"output/processed/country_wise_temp_{execution_date.strftime('%Y%m%d_%H%M%S')}.csv"
+    gcs_hook.copy(src_bucket, src_object, src_bucket, dest_object)
+    gcs_hook.delete(src_bucket, src_object)
+
 with DAG(
     'gcs_to_bq_dag',
     default_args=default_args,
-    description='A DAG to load CSV data from GCS to BigQuery',
+    description='A DAG to load CSV data from GCS to BigQuery and move to processed folder',
     schedule_interval='@daily',
+    catchup=False,
 ) as dag:
 
     load_csv_to_bq = GCSToBigQueryOperator(
@@ -26,13 +36,10 @@ with DAG(
         write_disposition='WRITE_APPEND',
     )
 
-    move_to_processed = GCSToGCSOperator(
+    move_to_processed = PythonOperator(
         task_id='move_to_processed',
-        source_bucket='firstworkflow',
-        source_object='output/country_wise_temp.csv',
-        destination_bucket='firstworkflow',
-        destination_object='output/processed/country_wise_temp_{{ ds_nodash }}_{{ ts_nodash }}.csv',
-        move_object=True,
+        python_callable=move_to_processed_func,
+        op_kwargs={'execution_date': '{{ execution_date }}'},
     )
 
     load_csv_to_bq >> move_to_processed
